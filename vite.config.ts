59620da78e08c5a18c5c468f003406aacd448cdf
printfCYQ <459987870@qq.com>
2022-11-19 09:33:27 +0800

    unplugin-auto & naiveUI & lint code

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,29 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
+import AutoImport from "unplugin-auto-import/vite";
+import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
+import Components from "unplugin-vue-components/vite";
+import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: [
+        "vue",
+        {
+          "naive-ui": [],
+        },
+      ],
+      dts: "src/auto-import.d.ts",
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()],
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
